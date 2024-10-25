edcad0991814ed61a86e23f166644f975e5377bb
Vladislav <forgamechepyx@mail.ru>
2024-10-25 16:48:49 +0700

    lab3ready

--- script/script.js
+++ script/script.js
@@ -34,3 +34,72 @@ function solveTask2() {
     }
 
 }
+
+function solveTask3_1() {
+    var ch1 = parseFloat(document.getElementById("ch1").value);
+    var ch2 = parseFloat(document.getElementById("ch2").value);
+    var ch3 = parseFloat(document.getElementById("ch3").value);
+
+    if (ch1 === ch2 || ch2 === ch3 || ch3 === ch1) {
+        document.getElementById("result3_1").textContent = `TRUE`;
+    }  
+    else {
+        document.getElementById("result3_1").textContent = `FALSE`;
+    }
+}
+
+function solveTask3_2() {
+    var ch1_2 = parseFloat(document.getElementById("ch1_2").value);
+    var ch2_2 = parseFloat(document.getElementById("ch2_2").value);
+    var ch3_2 = parseFloat(document.getElementById("ch3_2").value);
+
+    if (ch1_2 === ch2_2) {
+        document.getElementById("result3_2").textContent = `Порядковый номер отличного числа: 3`;
+    }  
+    else if (ch1_2 === ch3_2){
+        document.getElementById("result3_2").textContent = `Порядковый номер отличного числа: 2`;
+    }
+    else{
+        document.getElementById("result3_2").textContent = `Порядковый номер отличного числа: 1`;
+    }
+}
+
+function solveTask3_3() {
+    var X_3 = parseFloat(document.getElementById("X_3").value);
+    var result_3 = 0;
+
+    if ( X_3 >= 2 && X_3 <=5){
+        result_3 = X_3 + Math.cos(4.8*X_3);
+    }
+    else if (X_3 > 5){
+        result_3 = Math.log(Math.abs(X_3 + Math.sqrt(4.8*X_3)));
+    }
+    else if (X_3 < 2){
+        result_3 = Math.atan(0.64/((X_3)^2 + 1));
+    }
+    document.getElementById("result3_3").textContent = `При Х = ${X_3} Ответ: ${result_3}`;
+}
+
+function solveTask3_4() {
+    var X_4 = parseFloat(document.getElementById("X_4").value);
+    var result_4 = 0;
+
+    switch (X_4){
+        case 3:
+            result_4 = X_4 + Math.cos(4.8*X_4);
+            break;
+        case 6:
+            result_4 = Math.log(Math.abs(X_4 + Math.sqrt(4.8*X_4)));
+            break;
+        case 1:
+            result_4 = Math.atan(0.64/((X_4)^2 + 1));
+            break;
+        case 4:
+            result_4 = "Нет решения";
+            break
+        default:
+            result_4 = "При таком значении не решаем";
+    }
+
+    document.getElementById("result3_4").textContent = `При Х = ${X_4} Ответ: ${result_4}`;
+}
